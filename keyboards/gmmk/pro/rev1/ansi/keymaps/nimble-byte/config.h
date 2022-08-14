1dab12d106b6191de39777be76021533d7d858fb
Til Goepfert <tilgoepfert@gmail.com>
2022-08-14 18:17:25 +0200

    layer & led customisation
    - added function layer
    - added numpad layer
    - uppermost layer will be highlighted in magenta
    - led base color is a custom cyan

--- keyboards/gmmk/pro/rev1/ansi/keymaps/nimble-byte/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/nimble-byte/config.h
@@ -16,6 +16,78 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+// helper macro gracefully stolen from qmk_firmware/users/gourdo1/gourdo1.h
+#define ARRAYSIZE(arr) sizeof(arr) / sizeof(arr[0])
+
+// define additional colors
+
 #ifdef RGB_MATRIX_ENABLE
     #define RGB_DISABLE_WHEN_USB_SUSPENDED
+
+    #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
+    #define RGB_MATRIX_STARTUP_HUE 132
+    #define RGB_MATRIX_STARTUP_SAT 255
+    #define RGB_MATRIX_STARTUP_VAL 255
+    #define RGB_MATRIX_STARTUP_SPD 120
+
+    #undef ENABLE_RGB_MATRIX_ALPHAS_MODS                // Static dual hue, speed is hue for secondary hue
+    // #undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN           // Static gradient top to bottom, speed controls how much gradient changes
+    // #undef ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT        // Static gradient left to right, speed controls how much gradient changes
+    // #undef ENABLE_RGB_MATRIX_BREATHING                  // Single hue brightness cycling animation
+    #undef ENABLE_RGB_MATRIX_BAND_SAT                   // Single hue band fading saturation scrolling left to right
+    #undef ENABLE_RGB_MATRIX_BAND_VAL                   // Single hue band fading brightness scrolling left to right
+    #undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT          // Single hue 3 blade spinning pinwheel fades saturation
+    #undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL          // Single hue 3 blade spinning pinwheel fades brightness
+    #undef ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT            // Single hue spinning spiral fades saturation
+    #undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL            // Single hue spinning spiral fades brightness
+    #undef ENABLE_RGB_MATRIX_CYCLE_ALL                  // Full keyboard solid hue cycling through full gradient
+    #undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT           // Full gradient scrolling left to right
+    #undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN              // Full gradient scrolling top to bottom
+    // #undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON     // Full gradient scrolling out to in
+    #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN               // Full dual gradients scrolling out to in
+    #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL          // Full gradent Chevron shapped scrolling left to right
+    #undef ENABLE_RGB_MATRIX_CYCLE_PINWHEEL             // Full gradient spinning pinwheel around center of keyboard
+    #undef ENABLE_RGB_MATRIX_CYCLE_SPIRAL               // Full gradient spinning spiral around center of keyboard
+    #undef ENABLE_RGB_MATRIX_DUAL_BEACON                // Full gradient spinning around center of keyboard
+    // #undef ENABLE_RGB_MATRIX_RAINBOW_BEACON             // Full tighter gradient spinning around center of keyboard
+    #undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS          // Full dual gradients spinning two halfs of keyboard
+    #undef ENABLE_RGB_MATRIX_RAINDROPS                  // Randomly changes a single key's hue
+    #undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS        // Randomly changes a single key's hue and saturation
+    #undef ENABLE_RGB_MATRIX_HUE_BREATHING              // Hue shifts up a slight ammount at the same time, then shifts back
+    #undef ENABLE_RGB_MATRIX_HUE_PENDULUM               // Hue shifts up a slight ammount in a wave to the right, then back to the left
+    #undef ENABLE_RGB_MATRIX_HUE_WAVE                   // Hue shifts up a slight ammount and then back down in a wave to the right
+    #undef ENABLE_RGB_MATRIX_PIXEL_RAIN                 // Single hue fractal filled keys pulsing horizontally out to edges
+    #undef ENABLE_RGB_MATRIX_PIXEL_FLOW                 // Pulsing RGB flow along LED wiring with random hues
+    #undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL              // Randomly light keys with random hues
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif
--- keyboards/gmmk/pro/rev1/ansi/keymaps/nimble-byte/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/nimble-byte/keymap.c
@@ -16,13 +16,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-// clang-format off
-// custom names for layers
-enum userspace_layers {
-    QWERTY,
-    FNLAYER
+#include "rgb_matrix_map.h"
+
+// Custom Layers
+enum layers {
+    _BASE,
+    _FN,
+    _NUMPAD
 };
 
+#define TG_NUM TG(_NUMPAD)
+
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
@@ -44,31 +49,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // This keyboard defaults to 6KRO instead of NKRO for compatibility reasons (some KVMs and BIOSes are incompatible with NKRO).
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
-    [QWERTY] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_DEL,          KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_END,
-        KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    [_BASE] = LAYOUT(
+        KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME,          KC_MUTE,
+        KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
+        KC_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
+        KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
+        KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                             KC_RALT, MO(_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
-    [FNLAYER] = LAYOUT(
-        _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          KC_MPLY,
+    [_FN] = LAYOUT(
+        EE_CLR,  _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_END,           KC_MPLY,
         _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
-        KC_CAPS, _______, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
-        _______,          _______, RGB_HUI, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
+        _______, RGB_SAD, RGB_VAI, RGB_SAI, NK_TOGG, _______, _______, _______, _______, _______,  TG_NUM, _______, _______, QK_BOOT,          _______,
+        KC_CAPS, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______, KC_NLCK, _______, _______, _______, _______,          _______, RGB_MOD, _______,
+        _______, _______, _______,                            KC_MPLY,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
     ),
 
+    [_NUMPAD] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______,   KC_P7,   KC_P8,   KC_P9, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______,   KC_P4,   KC_P5,   KC_P6, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______,   KC_P1,   KC_P2,   KC_P3, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______,   KC_P0, KC_PDOT, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
 
+    /**
+    [X] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
+     */
+    ),
 };
 // clang-format on
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (IS_LAYER_ON(FNLAYER)) {
+    if (IS_LAYER_ON(_FN)) {
         if (clockwise) {
             tap_code(KC_MNXT);
         } else {
@@ -84,3 +106,71 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return false;
 }
 #endif // ENCODER_ENABLE
+
+#ifdef RGB_MATRIX_ENABLE
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (IS_HOST_LED_ON(USB_LED_CAPS_LOCK)) {
+        // TODO: find proper way to invert the current color
+        for (uint8_t i = 0; i < ARRAYSIZE(LED_SIDES_UPPER); i++) {
+            rgb_matrix_set_color(LED_SIDES_UPPER[i], RGB_BORDEAUX);
+        }
+        rgb_matrix_set_color(LED_CAPS, RGB_BORDEAUX);
+        rgb_matrix_set_color(LED_FN, RGB_BORDEAUX);
+    }
+
+    // light up sides in dark red, if numlock is OFF
+    if (!IS_HOST_LED_ON(USB_LED_NUM_LOCK)) {
+        for (uint8_t i = 0; i < ARRAYSIZE(LED_SIDES_LOWER); i++) {
+            rgb_matrix_set_color(LED_SIDES_LOWER[i], RGB_BORDEAUX);
+        }
+        rgb_matrix_set_color(LED_N, RGB_BORDEAUX);
+        rgb_matrix_set_color(LED_FN, RGB_BORDEAUX);
+    }
+
+    // LEDs are always blocked in rows, because I think about them that way
+    switch(get_highest_layer(layer_state)) {
+        case _FN:
+            rgb_matrix_set_color(LED_ESC, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_F5, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_F6, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_F7, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_F8, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_F9, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_F10, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_F11, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_HOME, RGB_DMAGENTA);
+
+            rgb_matrix_set_color(LED_1, RGB_DMAGENTA);
+
+            rgb_matrix_set_color(LED_Q, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_W, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_E, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_R, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_P, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_BSLS, RGB_DMAGENTA);
+
+
+            rgb_matrix_set_color(LED_CAPS, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_A, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_S, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_D, RGB_DMAGENTA);
+
+            rgb_matrix_set_color(LED_N, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_UP, RGB_DMAGENTA);
+
+            rgb_matrix_set_color(LED_SPC, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_LEFT, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_DOWN, RGB_DMAGENTA);
+            rgb_matrix_set_color(LED_RIGHT, RGB_DMAGENTA);
+            break;
+        case _NUMPAD:
+            for (uint8_t i = 0; i < ARRAYSIZE(LED_LIST_NUMPAD); i++) {
+                rgb_matrix_set_color(LED_LIST_NUMPAD[i], RGB_DMAGENTA);
+            }
+            break;
+        default:
+            // rgb_matrix_set_color(LED_F3, RGB_DMAGENTA);
+            break;
+    }
+}
+#endif // RGB_MATRIX_ENABLE
